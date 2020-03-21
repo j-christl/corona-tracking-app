f2d68c651defbe4b4a0868f4dbd0a92143574f11
Lukas Schröder <lukas.schroeder@tum.de>
2020-03-21 13:00:44 +0100

    Add keys for shared preferences in Strings.xml

--- CoronaTrackingApp/app/src/main/java/com/nikola/coronatrackingapp/QRScanActivity.java
+++ CoronaTrackingApp/app/src/main/java/com/nikola/coronatrackingapp/QRScanActivity.java
@@ -1,7 +1,16 @@
 package com.nikola.coronatrackingapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Handles scanning of QR-Codes and stores explored user-ids.
+TODO Find better places for user-ids than Shared Preferences
+ */
 
 public class QRScanActivity extends AppCompatActivity {
 
@@ -9,5 +18,32 @@ public class QRScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q_r_scan);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    private void storeUserId(String userId){
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.preferences),MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        Set<String> otherIds = preferences.getStringSet(getString(R.string.otherIds), null);
+
+        if (otherIds == null)
+            otherIds = new HashSet<>();
+
+        otherIds.add(userId);
+
+        editor.putStringSet(getString(R.string.otherIds), otherIds);
+        editor.commit();
     }
 }
