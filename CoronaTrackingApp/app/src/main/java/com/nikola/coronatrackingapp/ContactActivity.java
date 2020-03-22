457c3da876ecfbd377956a1574b9181b518568fa
Nikola <niktom@web.de>
2020-03-22 16:41:47 +0100

    implemented sending information when warned about possible infection

--- CoronaTrackingApp/app/src/main/java/com/nikola/coronatrackingapp/ContactActivity.java
+++ CoronaTrackingApp/app/src/main/java/com/nikola/coronatrackingapp/ContactActivity.java
@@ -1,7 +1,14 @@
 package com.nikola.coronatrackingapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutionException;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+    }
+
+    SharedPreferences preferences;
+
+    public void sendData(View view){
+        preferences = getApplicationContext().getSharedPreferences("MyPref",0);
+
+        EditText n = findViewById(R.id.sendFirstName);
+        String vorName = n.getText().toString();
+        n = findViewById(R.id.sendLastName);
+        String nachName = n.getText().toString();
+        EditText t = findViewById(R.id.sendNumber);
+        String telefonnummer = t.getText().toString();
+        String jwt = preferences.getString(getString(R.string.jwt),null);
+
+        boolean result=false;
+
+        try {
+             result = new AsyncSendInfectedInformation().execute(jwt, vorName, nachName, telefonnummer).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, (result ? "Informationen Hochgeladen" : "Hochladen fehlgeschlagen"), Toast.LENGTH_LONG).show();
+        Log.d("Corona-Tracking-App", "Result: " + result);
     }
 }
